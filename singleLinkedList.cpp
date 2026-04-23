16ce125272ebf2133c14fec8a308d79ceafa06b5
keysha-adesi <keyshaputrii590@gmail.com>
2026-04-23 11:27:33 +0700

    menambahkan pengecekan duplikasi NIM pada linked list

--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -40,6 +40,19 @@ void addNode()
         START = nodeBaru;
         return;
     }
-
+    Node *previous = START;
+    Node * current = START;
+    while (current != NULL && nim > current->noMhs)
+    {
+        if (nim == current->noMhs)
+        {
+            cout << "\nDuplikasi noMhs tidak  diijinkan\n";
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+    nodeBaru->next = current;
+    previous->next = nodeBaru;
 }
 }
