67a8a192fce4d54fa0f4decf1e1658b2dbcf2d42
keysha-adesi <keyshaputrii590@gmail.com>
2026-04-23 11:03:30 +0700

    menambahkan library

--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -1,1 +1,26 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+class Node
+{
+    public:
+    int noMhs;
+    Node *next;
+};
+
+class LinkedList
+{
+    Node *START;
+
+    public:
+LinkedList()
+{
+    START = NULL;
+}
+
+void addNode()
+{
+    
+}
+}
