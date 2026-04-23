2fb5bc5cbf94aa576b33c77450ee696bc8d167dc
keysha-adesi <keyshaputrii590@gmail.com>
2026-04-23 11:45:32 +0700

    menambahkan fungsi delNode untuk menghapus node berdasrkan NIM

--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -71,5 +71,28 @@ bool search (int nim, Node *&previous, Nude *&current)
 
     }
     return (current != NULL);
+
+}
+bool delNode(int nim)
+{
+    Node *current, *previous;
+
+    if (!search(nim,previous,current))
+    return false;
+
+    if (current ==START)
+    START = START->next;
+    else
+    previous->next = current->next;
+    delete current;
+    return true;
+}
+void traverse()
+{
+    if (listEmpty())
+    {
+        cout << "\nList Kosong\n"
+    }
+    else
 }
 }
